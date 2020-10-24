8fc707563471c085e8f6445a631fd505c5a2a81d
ctruciosm <ctrucios@gmail.com>
2020-10-24 08:57:52 -0300

    ForeComb

--- ForeComb.R
+++ ForeComb.R
@@ -5,6 +5,8 @@
 library(psych)
 library(Rsolnp)
 library(dplyr)
+library(stringr)  
+source("Optimizations.R")
 setwd("./Data/BTC/")
 #setwd("./Data/ETH/")
 #setwd("./Data/LTC/")
@@ -21,9 +23,31 @@ ES2 = as.matrix(read.csv("ES.csv")[,c("GAS2","MSGARCH2","Boot2")])+mu
 VaR5 = as.matrix(read.csv("VaR.csv")[,c("GAS5","MSGARCH5","Boot5")])+mu
 ES5 = as.matrix(read.csv("ES.csv")[,c("GAS5","MSGARCH5","Boot5")])+mu
 
-crypto = read.csv("BTCUSDT-1d-data.csv") %>% 
-  mutate(date = as.Date(timestamp)) %>% arrange(date) %>% mutate(ret = c(0,diff(log(close))*100)) %>% 
-  dplyr::select(date, ret) %>% filter(date > "2017-08-17", date < "2020-08-18")
+
+if(str_sub(getwd(), - 3, - 1)   == "BTC"){
+  crypto = read.csv("BTCUSDT-1d-data.csv") %>% 
+    mutate(date = as.Date(timestamp)) %>% arrange(date) %>% mutate(ret = c(0,diff(log(close))*100)) %>% 
+    dplyr::select(date, ret) %>% filter(date > "2017-08-17", date < "2020-09-18")
+}
+
+if(str_sub(getwd(), - 3, - 1)   == "ETH"){
+  crypto = read.csv("ETHUSDT-1d-data.csv") %>% 
+    mutate(date = as.Date(timestamp)) %>% arrange(date) %>% mutate(ret = c(0,diff(log(close))*100)) %>% 
+    dplyr::select(date, ret) %>% filter(date > "2017-08-17", date < "2020-09-18")
+}
+
+if(str_sub(getwd(), - 3, - 1)  == "LTC"){
+  crypto = read.csv("LTCUSDT-1d-data.csv") %>% 
+    mutate(date = as.Date(timestamp)) %>% arrange(date) %>% mutate(ret = c(0,diff(log(close))*100)) %>% 
+    dplyr::select(date, ret) %>% filter(date > "2017-12-13", date < "2020-09-18")
+}
+
+if(str_sub(getwd(), - 3, - 1)  == "XRP"){
+  crypto =  read.csv("XRPUSDT-1d-data.csv") %>% 
+    mutate(date = as.Date(timestamp)) %>% arrange(date) %>% mutate(ret = c(0,diff(log(close))*100)) %>% 
+    dplyr::select(date, ret) %>% filter(date > "2018-05-04", date < "2020-09-18") 
+}
+
 
 # Load InS Data
 
@@ -71,18 +95,28 @@ omega1MSC = omega2MSC = omega5MSC = matrix(0,ncol=2*M,nrow = length(ret))
 for (j in 1:3){
   if (j == 1) S_ = FZG
   if (j == 2) S_ = NZ
-  if (j == 3) S_= AL
+  if (j == 3) S_ = AL
 
+  # Initial values RSC
+  lini1 = 0.000001
+  lini2 = 0.000001
+  lini5 = 0.000001
+  # Initial values MSC
+  omegaini1 = rep(rep(1/M,M),2)
+  omegaini2 = rep(rep(1/M,M),2)
+  omegaini5 = rep(rep(1/M,M),2)  
+  
     for (i in 1:OoS){
       #### RSC  
-      lambda1 = RSC_opt(VaR = cbind(inVaR1_Boot[,i],inVaR1_GAS[,i], inVaR1_MS[,i]), ES = cbind(inES1_Boot[,i],inES1_GAS[,i], inES1_MS[,i]), r =  crypto$ret[i:(InS+i-1)], alpha = a1, S = S_)
-      lambda2 = RSC_opt(VaR = cbind(inVaR2_Boot[,i],inVaR2_GAS[,i], inVaR2_MS[,i]), ES = cbind(inES2_Boot[,i],inES2_GAS[,i], inES2_MS[,i]), r =  crypto$ret[i:(InS+i-1)], alpha = a2, S = S_)
-      lambda5 = RSC_opt(VaR = cbind(inVaR5_Boot[,i],inVaR5_GAS[,i], inVaR5_MS[,i]), ES = cbind(inES5_Boot[,i],inES5_GAS[,i], inES5_MS[,i]), r =  crypto$ret[i:(InS+i-1)], alpha = a5, S = S_)
-      
+      lambda1 = RSC_opt(parini = lini1, VaR = cbind(inVaR1_Boot[,i],inVaR1_GAS[,i], inVaR1_MS[,i]), ES = cbind(inES1_Boot[,i],inES1_GAS[,i], inES1_MS[,i]), r =  crypto$ret[i:(InS+i-1)], alpha = a1, S = S_)
+      lini1 = lambda1
+      lambda2 = RSC_opt(parini = lini2, VaR = cbind(inVaR2_Boot[,i],inVaR2_GAS[,i], inVaR2_MS[,i]), ES = cbind(inES2_Boot[,i],inES2_GAS[,i], inES2_MS[,i]), r =  crypto$ret[i:(InS+i-1)], alpha = a2, S = S_)
+      lini2 = lambda2
+      lambda5 = RSC_opt(parini = lini5, VaR = cbind(inVaR5_Boot[,i],inVaR5_GAS[,i], inVaR5_MS[,i]), ES = cbind(inES5_Boot[,i],inES5_GAS[,i], inES5_MS[,i]), r =  crypto$ret[i:(InS+i-1)], alpha = a5, S = S_)
+      lini5 = lambda5
       omega1RSC[i,] = RSC_Eval(lambda1, VaR = cbind(inVaR1_Boot[,i],inVaR1_GAS[,i], inVaR1_MS[,i]), ES = cbind(inES1_Boot[,i],inES1_GAS[,i], inES1_MS[,i]), r = crypto$ret[i:(InS+i-1)], alpha = a1, S = S_)
       omega2RSC[i,] = RSC_Eval(lambda2, VaR = cbind(inVaR2_Boot[,i],inVaR2_GAS[,i], inVaR2_MS[,i]), ES = cbind(inES2_Boot[,i],inES2_GAS[,i], inES2_MS[,i]), r = crypto$ret[i:(InS+i-1)], alpha = a2, S = S_)
       omega5RSC[i,] = RSC_Eval(lambda5, VaR = cbind(inVaR5_Boot[,i],inVaR5_GAS[,i], inVaR5_MS[,i]), ES = cbind(inES5_Boot[,i],inES5_GAS[,i], inES5_MS[,i]), r = crypto$ret[i:(InS+i-1)], alpha = a5, S = S_)
-      
       VaR_RSC[i,] = c(VaR1[i,]%*%omega1RSC[i,],VaR2[i,]%*%omega2RSC[i,],VaR5[i,]%*%omega5RSC[i,])
       ES_RSC[i,] = c(ES1[i,]%*%omega1RSC[i,],ES2[i,]%*%omega2RSC[i,],ES5[i,]%*%omega5RSC[i,])   
       
@@ -92,16 +126,17 @@ for (j in 1:3){
       ES_AVG[i,] = c(ES1[i,]%*%EW,ES2[i,]%*%EW,ES5[i,]%*%EW)   
       
       #### MSC
-      omega1MSC[i,] = MSC_opt(VaR = cbind(inVaR1_Boot[,i],inVaR1_GAS[,i], inVaR1_MS[,i]), ES = cbind(inES1_Boot[,i],inES1_GAS[,i], inES1_MS[,i]), r = crypto$ret[i:(InS+i-1)], alpha = a1, S = S_)
-      omega2MSC[i,] = MSC_opt(VaR = cbind(inVaR2_Boot[,i],inVaR2_GAS[,i], inVaR2_MS[,i]), ES = cbind(inES2_Boot[,i],inES2_GAS[,i], inES2_MS[,i]), r = crypto$ret[i:(InS+i-1)], alpha = a2, S = S_)
-      omega5MSC[i,] = MSC_opt(VaR = cbind(inVaR5_Boot[,i],inVaR5_GAS[,i], inVaR5_MS[,i]), ES = cbind(inES5_Boot[,i],inES5_GAS[,i], inES5_MS[,i]), r = crypto$ret[i:(InS+i-1)], alpha = a5, S = S_)
-      
+      omega1MSC[i,] = MSC_opt(parini = omegaini1, VaR = cbind(inVaR1_Boot[,i],inVaR1_GAS[,i], inVaR1_MS[,i]), ES = cbind(inES1_Boot[,i],inES1_GAS[,i], inES1_MS[,i]), r = crypto$ret[i:(InS+i-1)], alpha = a1, S = S_)
+      omega2MSC[i,] = MSC_opt(parini = omegaini2, VaR = cbind(inVaR2_Boot[,i],inVaR2_GAS[,i], inVaR2_MS[,i]), ES = cbind(inES2_Boot[,i],inES2_GAS[,i], inES2_MS[,i]), r = crypto$ret[i:(InS+i-1)], alpha = a2, S = S_)
+      omega5MSC[i,] = MSC_opt(parini = omegaini5, VaR = cbind(inVaR5_Boot[,i],inVaR5_GAS[,i], inVaR5_MS[,i]), ES = cbind(inES5_Boot[,i],inES5_GAS[,i], inES5_MS[,i]), r = crypto$ret[i:(InS+i-1)], alpha = a5, S = S_)
+      omegaini1 = omega1MSC[i,]
+      omegaini2 = omega2MSC[i,]
+      omegaini5 = omega5MSC[i,]
       VaR_MSC[i,] = c(VaR1[i,]%*%omega1MSC[i,1:M],VaR2[i,]%*%omega2MSC[i,1:M],VaR5[i,]%*%omega5MSC[i,1:M])
       ES_MSC[i,] = c(ES1[i,]%*%omega1MSC[i,(M+1):(2*M)],ES2[i,]%*%omega2MSC[i,(M+1):(2*M)],ES5[i,]%*%omega5MSC[i,(M+1):(2*M)])  
-      
     }
   
-  
+  # Once we compute the combining estimators, we save the results
   if (j == 1) {
     write.table(omega1RSC,"omega1_RSC_FZG.csv")
     write.table(omega2RSC,"omega2_RSC_FZG.csv")
@@ -120,7 +155,6 @@ for (j in 1:3){
     write.table(VaR_MSC,"VaR_MSC_FZG.csv")
     write.table(ES_MSC,"ES_MSC_FZG.csv")
   }
-  
   if (j == 2) {
     write.table(omega1RSC,"omega1_RSC_NZ.csv")
     write.table(omega2RSC,"omega2_RSC_NZ.csv")
@@ -139,7 +173,6 @@ for (j in 1:3){
     write.table(VaR_MSC,"VaR_MSC_NZ.csv")
     write.table(ES_MSC,"ES_MSC_NZ.csv")
   }
-  
   if (j == 3) {
     write.table(omega1RSC,"omega1_RSC_AL.csv")
     write.table(omega2RSC,"omega2_RSC_AL.csv")
@@ -158,8 +191,6 @@ for (j in 1:3){
     write.table(VaR_MSC,"VaR_MSC_AL.csv")
     write.table(ES_MSC,"ES_MSC_AL.csv")
   }
-
-
 }
 
 
